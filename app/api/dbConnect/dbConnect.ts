e0934783e86e14e0f94447bda3f0028da5c0bbdd
wjrelumba <92615653+wjrelumba@users.noreply.github.com>
2024-02-15 02:21:54 +0800

    Some finishing touches, create new team design, profile page

--- app/api/dbConnect/dbConnect.ts
+++ app/api/dbConnect/dbConnect.ts
@@ -1,12 +1,18 @@
 import { ConnectionOptions } from "mysql2"
 
 const DBConnect = () => {
+    // const conn: ConnectionOptions = {
+    //     user: "sql6684135",
+    //     host: "sql6.freesqldatabase.com",
+    //     database: "sql6684135",
+    //     password: "KAETCWFjeg",
+    //     port: 3306
+    // }
     const conn: ConnectionOptions = {
-        user: "sql6684135",
-        host: "sql6.freesqldatabase.com",
-        database: "sql6684135",
-        password: "KAETCWFjeg",
-        port: 3306
+        user: "root",
+        host: "localhost",
+        database: "synchdb",
+        password: "",
     }
     return(
         conn
