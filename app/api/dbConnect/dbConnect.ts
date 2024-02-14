d16613aa0beb3e94262762bb188330c843db366d
wjrelumba <92615653+wjrelumba@users.noreply.github.com>
2024-02-14 22:08:24 +0800

    Database config changed to access live hosting mysql

--- app/api/dbConnect/dbConnect.ts
+++ app/api/dbConnect/dbConnect.ts
@@ -2,9 +2,11 @@ import { ConnectionOptions } from "mysql2"
 
 const DBConnect = () => {
     const conn: ConnectionOptions = {
-        user: "root",
-        host: "localhost",
-        database: "synchdb"
+        user: "sql6684135",
+        host: "sql6.freesqldatabase.com",
+        database: "sql6684135",
+        password: "KAETCWFjeg",
+        port: 3306
     }
     return(
         conn
