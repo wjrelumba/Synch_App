73d35ee56e93d39ef130ec87b6f3bff5178be6b5
wjrelumba <92615653+wjrelumba@users.noreply.github.com>
2024-02-15 11:42:37 +0800

    Create team bug, fixed

--- app/createNewTeam/page.tsx
+++ app/createNewTeam/page.tsx
@@ -42,9 +42,9 @@ export default function CreateTeamForm() {
                 <div className={`bg-synchGray-50 rounded-lg p-8 shadow-lg transform transition-transform duration-500 ease-in-out ${showForm ? 'translate-y-0' : 'translate-y-full'}`}>
                     <h2 className="text-3xl font-mono mb-4">Create Team</h2>
                     <label htmlFor="teamName" className="block font-mono mb-2">Team Name:</label>
-                    <input type="text" className="border border-synchGray-100 rounded px-4 py-2 mb-4 w-full" onBlur={teamNameHandler} placeholder="Team Name" required />
+                    <input type="text" className="border text-black border-synchGray-100 rounded px-4 py-2 mb-4 w-full" onBlur={teamNameHandler} placeholder="Team Name" required />
                     <label htmlFor="teamID" className="block font-mono mb-2">Team ID:</label>
-                    <input type="text" className="border border-gray-300 rounded px-4 py-2 mb-4 w-full" onBlur={teamIDHandler} placeholder="Team ID" required />
+                    <input type="text" className="border text-black border-gray-300 rounded px-4 py-2 mb-4 w-full" onBlur={teamIDHandler} placeholder="Team ID" required />
                     <button onClick={btnFunc} className="bg-synchBlue-50 hover:bg-synchBlue-100 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-4">Create Team</button>
                     <button onClick={cnclBtn} className="bg-synchGray-150 hover:bg-synchGray-200 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Cancel</button>
                 </div>
